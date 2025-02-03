fa2870de2c4d3188a126e76a9d0afc052a1af007
dagit2008 <dfederico.alzate@udea.edu.co>
2025-02-03 12:58:52 -0500

    second commit

---
+++ index.py
@@ -0,0 +1,11 @@
+# Pedir la edad al usuario
+edad = int(input("¿Cuál es tu edad? "))
+
+# El segundo número primo es 3
+segundo_primo = 3
+
+# Realizar la multiplicación
+resultado = edad * segundo_primo
+
+# Mostrar el resultado
+print(f"Si multiplicamos tu edad por el segundo número primo (3), el resultado es: {resultado}")
